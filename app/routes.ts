c70d77411e8b63f74ce430195021262ac54dcd63
shrusht-vaghasiya <shrushti@kanhasoft.com>
2026-05-06 13:04:32 +0530

    modify root level routes.ts file

--- app/routes.ts
+++ app/routes.ts
@@ -1,15 +1,30 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, route, layout, prefix } from "@react-router/dev/routes";
 
 export default [
-    index("routes/_app._index.tsx"),
-    route("login", "routes/_auth.login.tsx"),
-    route("signup", "routes/_auth.signup.tsx"),
+    index("routes/_index.tsx"),
     route("logout", "routes/logout.tsx"),
-    route("profile", "routes/_app.profile.tsx"),
-    route("todos", "routes/_app.todos.tsx"),
-    route("todos/new", "routes/_app.todos.new.tsx"),
-    route("todos/:taskId", "routes/_app.todos.$taskId.tsx"),
-    route("todos/:taskId/edit", "routes/_app.todos.$taskId.edit.tsx"),
-    route("todos/:taskId/toggle", "routes/_app.todos.$taskId.toggle.tsx"),
-    route("todos/:taskId/delete", "routes/_app.todos.$taskId.delete.tsx"),
+    route("*", "routes/[...404].tsx"),
+
+    // Public routes
+    layout("routes/_auth/_layout.tsx", [
+        route("login", "routes/_auth/login.tsx"),
+        route("signup", "routes/_auth/signup.tsx"),
+    ]),
+
+    // Protected routes
+    layout("routes/_app/_layout.tsx", [
+        route("profile", "routes/_app/profile.tsx"),
+
+        ...prefix("todos", [
+            index("routes/_app/todos/_index.tsx"),
+            route("new", "routes/_app/todos/new.tsx"),
+
+            ...prefix(":taskId", [
+                index("routes/_app/todos/$taskId/_index.tsx"),
+                route("edit", "routes/_app/todos/$taskId/edit.tsx"),
+                route("toggle", "routes/_app/todos/$taskId/toggle.tsx"),
+                route("delete", "routes/_app/todos/$taskId/delete.tsx"),
+            ]),
+        ]),
+    ]),
 ] satisfies RouteConfig;
